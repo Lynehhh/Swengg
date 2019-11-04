e183826574acfde9fe0291ce0904c90ad33f1645
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-11-05 00:53:43 +0800

    Update mehh

--- home.php
+++ home.php
@@ -27,6 +27,10 @@
   <!-- FAVICON -->
   <link href="img/favicon.png" rel="shortcut icon">
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -34,10 +38,7 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
     <style>
-        select {
-            padding: 30px;
-            min-height: 50px;
-        }
+
     </style>
     
 
