1952eb3751e44295ec100f91a217726bb5817463
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-11-23 17:09:37 +0800

    Update topbar.php

--- topbar.php
+++ topbar.php
@@ -54,7 +54,7 @@ $userType = $_SESSION['user_type'];
 			<div class="col-md-12">
 				<nav class="navbar navbar-expand-lg  navigation">
 					<a class="navbar-brand" href="home.php">
-						<img src="images/logo.png" alt="" style="width: 50%;">
+						<img src="images/logo.png" alt="" style="width: 70%;">
 					</a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
