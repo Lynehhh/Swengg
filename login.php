1da8211da8ca760cc34b6085df547cba6c897948
azulahime <49381282+azulahime@users.noreply.github.com>
2019-12-02 16:58:00 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,44 +1,39 @@
 <?php
 session_start();
 require_once('connection.php');
+	
+if (isset($_POST['Login']))
 
-	if (isset($_POST['Login']))
-
+{
+	
+	if (empty($_POST['email']) || empty ($_POST['password']))
 	{
-        
-		if (empty($_POST['email']) || empty ($_POST['password']))
-		{
-			header("location:login.php?Empty=Please Fill in the blanks.");
+		header("location:login.php?Empty=Please Fill in the blanks.");
+	}
+	else
+	{
+		
+		
+		$query="select * from users where email='".$_POST['email']."' and password='".$_POST['password']."'";
+		$result = mysqli_query($con,$query);
+		
+		
+		if($result = $con->query($query)){
+			if ($result->num_rows > 0) { 
+				while($row = $result->fetch_assoc()) {
+					$_SESSION['email'] = $row['email'];
+					$_SESSION["user_type"] = $row["usertype"];
+				}
+			}
+			header("location:home.php"); 
 		}
 		else
 		{
-            
-            
-			$query="select * from users where email='".$_POST['email']."' and password='".$_POST['password']."'";
-            $result = mysqli_query($con,$query);
-			
-
-			if (mysqli_fetch_assoc($result))
-			{
-                $_SESSION['email'] = $_POST['email'];
-               // $userQuery = "select usertype from users where email='".$_POST['email']."'";
-                //$userType =	 mysqli_fetch_row(mysqli_query($con, $userQuery));
-
-
-              //  if($userType[0]== '2'){
-                header("location:viewlistings.php"); // indicate customized file location
-              //  }
-               
-
-			}
-			else
-			{
-                header("location:login.php?Invalid=Invalid Username and Password");
-            
-			}
+			header("location:login.php?Invalid=Invalid Username and Password");
+		
 		}
-    }
-    
+	}
+}
 	
 ?>
 
