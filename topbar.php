63d94366869e6be562d16b7bbb3b0a50cf745d05
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-12-03 13:42:25 +0800

    Update Me

--- topbar.php
+++ topbar.php
@@ -1,8 +1,13 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
 session_start();
 require_once('connection.php');
+
+
+
 $userType = $_SESSION['user_type'];
-    
+$email = $_SESSION['email'];
+
 ?>
 
 <!DOCTYPE html>
@@ -35,7 +40,14 @@ $userType = $_SESSION['user_type'];
 
   <!-- FAVICON -->
   <link href="img/favicon.png" rel="shortcut icon">
-	
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+    
     <style>
         .mb-20 {
         margin-bottom: 20px;
@@ -45,53 +57,47 @@ $userType = $_SESSION['user_type'];
         }
 
     </style>
-
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
 </head>
 
 <body>
-    <section class="" style="height: 85px; background-color: #020000;">
+    <section style="height: 85px; background-image: linear-gradient(to right, #187e7e, #093637, #051b1b)">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
                 <a class="navbar-brand" href="home.php">
-						<img src="images/logo.png" alt="" style="height: 170%;">
-					</a>
+                    <img src="images/logo.png" alt="" style="height: 170%;">
+                </a>
 				<nav class="navbar navbar-expand-lg  navigation">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+					</button>
+					<?php if($email != "" ){?>
+
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav ml-auto main-nav ">
-							<li class="nav-item ">
+							<li class="nav-item">
 								<a class="nav-link top-link" href="home.php">Home</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link top-link" href="new_catalog.php">Car Catalog</a>
 							</li>
+							<?php if($userType == "Owner" ){?>
 							<li class="nav-item dropdown dropdown-slide">
 								<a class="nav-link top-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Pages <span><i class="fa fa-angle-down"></i></span>
+									Owner Pages <span><i class="fa fa-angle-down"></i></span>
 								</a>
 								<!-- Dropdown list -->
 								<div class="dropdown-menu dropdown-menu-right">
-									<a class="dropdown-item" href="category.html">Category</a>
-									<a class="dropdown-item" href="car_details.php">Car Details</a>
-									<a class="dropdown-item" href="slisting_new.php">New Listing</a>
-									<a class="dropdown-item" href="sviewlisting.php">Listing</a>
-									<a class="dropdown-item" href="user-profile.html">User Profile</a>
-									<a class="dropdown-item" href="submit-coupon.html">Submit Coupon</a>
-									<a class="dropdown-item" href="blog.html">Blog</a>
-									<a class="dropdown-item" href="single-blog.html">Single Post</a>
+									<a class="dropdown-item" href="sviewlisting.php">My Listings</a>
+									<a class="dropdown-item" href="listing.php">Add New Listing</a>
 								</div>
 							</li>
+							<?php } ?>
 							<li class="nav-item dropdown dropdown-slide">
 								<a class="nav-link top-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									My Transactions <span><i class="fa fa-angle-down"></i></span>
+                                    My Transactions <span><i class="fa fa-angle-down"></i></span></a>
 
-									<div class="dropdown-menu dropdown-menu-right">
+                                <div class="dropdown-menu dropdown-menu-right">
 									<?php if($userType == "Renter" ){?>
 									<a class="dropdown-item" href="transactions_pending_requests.php">Renter Transactions</a>
 									<?php } else{?>
@@ -100,8 +106,19 @@ $userType = $_SESSION['user_type'];
 									<?php } ?>
 								</div>
 							</li>
+                            <li class="nav-item dropdown dropdown-slide">
+								<a class="nav-link top-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    My Profile <span><i class="fa fa-angle-down"></i></span></a>
+                                <div class="dropdown-menu dropdown-menu-right">
+									<a class="dropdown-item" href="">View Profile</a>
+									<a class="dropdown-item" href="">Log Out</a>
+								</div>
+							</li>
 
 						</ul>
+						<?php } ?>
+
+						<?php if($email == "" ){?>
 						<ul class="navbar-nav ml-auto mt-10">
 							<li class="nav-item">
 								<a class="nav-link top-link login-button" href="login.php">Login</a>
@@ -110,6 +127,8 @@ $userType = $_SESSION['user_type'];
 								<a class="nav-link add-button" href="signup.php"><i class="fa fa-plus-circle"></i> Sign In</a>
 							</li>
 						</ul>
+						<?php } ?>
+
 					</div>
 				</nav>
 			</div>
