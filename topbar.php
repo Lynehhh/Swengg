2473482b29d541f1412fd99885fd4874b1f58de2
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-12-03 13:34:49 +0800

    update me

--- topbar.php
+++ topbar.php
@@ -1,13 +1,8 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
 session_start();
 require_once('connection.php');
-
-
-
 $userType = $_SESSION['user_type'];
-$email = $_SESSION['email'];
-
+    
 ?>
 
 <!DOCTYPE html>
@@ -40,6 +35,16 @@ $email = $_SESSION['email'];
 
   <!-- FAVICON -->
   <link href="img/favicon.png" rel="shortcut icon">
+	
+    <style>
+        .mb-20 {
+        margin-bottom: 20px;
+        }
+        .top-link {
+        color: white;
+        }
+
+    </style>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -50,41 +55,40 @@ $email = $_SESSION['email'];
 </head>
 
 <body>
-    <section>
+    <section class="" style="height: 85px; background-color: #020000;">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<nav class="navbar navbar-expand-lg  navigation">
-					<a class="navbar-brand" href="home.php">
-						<img src="images/logo.png" alt="" style="width: 50%;">
+                <a class="navbar-brand" href="home.php">
+						<img src="images/logo.png" alt="" style="height: 170%;">
 					</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-					</button>
-					<?php if($email != "" ){?>
-
+				<nav class="navbar navbar-expand-lg  navigation">
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav ml-auto main-nav ">
-							<li class="nav-item">
-								<a class="nav-link" href="home.php">Home</a>
+							<li class="nav-item ">
+								<a class="nav-link top-link" href="home.php">Home</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link" href="new_catalog.php">Car Catalog</a>
+								<a class="nav-link top-link" href="new_catalog.php">Car Catalog</a>
 							</li>
-							<?php if($userType == "Owner" ){?>
 							<li class="nav-item dropdown dropdown-slide">
-								<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Owner Pages <span><i class="fa fa-angle-down"></i></span>
+								<a class="nav-link top-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Pages <span><i class="fa fa-angle-down"></i></span>
 								</a>
 								<!-- Dropdown list -->
 								<div class="dropdown-menu dropdown-menu-right">
-									<a class="dropdown-item" href="sviewlisting.php">My Listings</a>
-									<a class="dropdown-item" href="listing.php">Add New Listing</a>
+									<a class="dropdown-item" href="category.html">Category</a>
+									<a class="dropdown-item" href="car_details.php">Car Details</a>
+									<a class="dropdown-item" href="slisting_new.php">New Listing</a>
+									<a class="dropdown-item" href="sviewlisting.php">Listing</a>
+									<a class="dropdown-item" href="user-profile.html">User Profile</a>
+									<a class="dropdown-item" href="submit-coupon.html">Submit Coupon</a>
+									<a class="dropdown-item" href="blog.html">Blog</a>
+									<a class="dropdown-item" href="single-blog.html">Single Post</a>
 								</div>
 							</li>
-							<?php } ?>
 							<li class="nav-item dropdown dropdown-slide">
-								<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<a class="nav-link top-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 									My Transactions <span><i class="fa fa-angle-down"></i></span>
 
 									<div class="dropdown-menu dropdown-menu-right">
@@ -98,19 +102,14 @@ $email = $_SESSION['email'];
 							</li>
 
 						</ul>
-						<?php } ?>
-
-						<?php if($email == "" ){?>
 						<ul class="navbar-nav ml-auto mt-10">
 							<li class="nav-item">
-								<a class="nav-link login-button" href="login.php">Login</a>
+								<a class="nav-link top-link login-button" href="login.php">Login</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link add-button" href="signup.php"><i class="fa fa-plus-circle"></i> Sign In</a>
 							</li>
 						</ul>
-						<?php } ?>
-
 					</div>
 				</nav>
 			</div>
