4cc0f103de683b93ab8bbe27f714f8bee55ef0b7
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-12-11 14:22:35 +0800

    fixed the photo part

--- update_profile.php
+++ update_profile.php
@@ -67,59 +67,12 @@ $usertype =  $_SESSION['user_type'];
                 <div class="col-md-10 offset-md-1 col-lg-6 offset-lg-0">
                     <div class="widget user-dashboard-menu">
                         <h3 class="widget-header user mb-20">User Photo</h3>
-                        <label for="comunity-name">Upload Images</label>
-
-                        <!-- Carousel -->
-                        <div class=" mb-20">
-                          <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                            <!-- Indicators -->
-                            <ol class="carousel-indicators">
-                              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                              <li data-target="#myCarousel" data-slide-to="1"></li>
-                              <li data-target="#myCarousel" data-slide-to="2"></li>
-                            </ol>
-
-                            <!-- Wrapper/ Images for slides -->
-                            <div class="carousel-inner">
-                            <?php 
-                                $ctr = 0;
-                                $query= "SELECT location FROM car_images WHERE carID =" .$carID;
-                        $result =  $con->query($query);
-                    if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        $ctr = $ctr + 1;
-                        if($ctr == 1){
-                            echo '<div class="item active">
-                                <img src="'.$row['location'].'" alt="Photo '.$ctr.'" style="width:100%; height: 300px;">
-                              </div>';
-                        }
-                        else{
-                            echo '<div class="item">
-                                <img src="'.$row['location'].'" alt="Photo '.$ctr.'" style="width:100%; height: 300px;">
-                              </div>';
-                        }
-                    
-            }
-        }
-                                ?>
-
-                              
-                            </div>
-
-                            <!-- Left and right controls -->
-                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                              <span class="glyphicon glyphicon-chevron-left"></span>
-                              <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                              <span class="glyphicon glyphicon-chevron-right"></span>
-                              <span class="sr-only">Next</span>
-                            </a>
-                          </div>
-                        </div>
+                        <!--  Image -->
+                        <img src="images/user/user-thumb.jpg" style="width:auto;height:250px;">
                         
                         <!-- Upload Image -->
-                        <div class="form-group choose-file mb-20">
+                        <div class="form-group choose-file mt-30">
+                            <h5 for="comunity-name">Upload Images</h5>
                             <input type="file" name="files[]" class="form-control-file d-inline" multiple>
                          </div>
                     </div>
@@ -166,13 +119,13 @@ $usertype =  $_SESSION['user_type'];
                         <!-- Buttons -->
                         <div class="row">
                             <div class="col-md-10 offset-md-1 col-lg-4 offset-lg-0">
-                                <button class="btn btn-success" style="width: 100%;" type = "submit" name="update" value="Update" formaction = "processeditprofile.php">Update</button>
+                                <button class="btn btn-success" type = "submit" name="update" value="Update" formaction = "processeditprofile.php">Update</button>
                             </div>
                             <div class="col-md-10 offset-md-1 col-lg-4 offset-lg-0">
-                                <button class="btn btn-danger" style="width: 100%;" name= "upgrade" formaction = "processupgrade.php">Upgrade</button>
+                                <button class="btn btn-danger" name= "upgrade" formaction = "processupgrade.php">Upgrade</button>
                             </div>
                             <div class="col-md-10 offset-md-1 col-lg-4 offset-lg-0">
-                                <button type="button" class="btn btn-primary" style="width: 100%;" onclick="history.back()">Back</button>
+                                <button type="button" class="btn btn-primary"onclick="history.back()">Back</button>
                             </div>
                         </div>
                     </div>
