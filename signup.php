0bca3fcd6faeb758294f84e303496a1e1e082dc0
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-12-13 02:01:14 +0800

    ok naa

--- signup.php
+++ signup.php
@@ -31,21 +31,23 @@
 
         <section class="signup">
             <!-- <img src="images/signup-bg.jpg" alt=""> -->
-            <div class="container">
-                <div class="signup-content">
+            <div class="container" style="width: 900px;">
+                <div class="signup-content" style="padding: 10px 45px;">
                     <form method="POST" id="signup-form" class="signup-form">
-                        <h2 class="form-title">Create account</h2>
+                        <h2 class="form-title mb-20">Create account</h2>
                         <div class="row">
-                            <div class="col-md-4 offset-md-1 col-lg-9 offset-lg-0">
+                            <div class="col-md-4 offset-md-1 col-lg-12 offset-lg-0">
                                 
                                     <!-- First Name -->
                                     <div class="col-lg-6 col-md-12">
+                                        <label for="comunity-name">First Name</label>
                                         <div class="form-group">
                                             <input type="text" class="form-input" name="name" id="name" placeholder="First Name"/>
                                         </div>
                                     </div>
                                     <!-- Last Name -->
                                     <div class="col-lg-6 col-md-12">
+                                        <label for="comunity-name">Last Name</label>
                                         <div class="form-group">
                                             <input type="text" class="form-input" name="name" id="name" placeholder="Last Name"/>
                                         </div>
@@ -53,54 +55,66 @@
                                 
                                 <!-- Email -->
                                 <div class="col-lg-12 col-md-12">
+                                    <label for="comunity-name">Email</label>
                                     <div class="form-group">
                                         <input type="email" class="form-input" name="email" id="email" placeholder="Your Email"/>
                                     </div>
                                 </div>
                                 <!-- Password -->
                                 <div class="col-lg-12 col-md-12">
+                                    <label for="comunity-name">Password</label>
                                     <div class="form-group">
                                         <input type="password" class="form-input" name="password" id="password" placeholder="Password"/>
                                     </div>
                                 </div>
                                 <!-- Date of Birth -->
                                 <div class="col-lg-6 col-md-12">
+                                    <label for="comunity-name">Date of Birth</label>
                                     <div class="form-group">
                                         <input type="date" class="form-input" name="" id="" placeholder="Date of Birth"/>
                                     </div>
                                 </div>
                                 <!-- Contact -->
                                 <div class="col-lg-6 col-md-12">
+                                    <label for="comunity-name">Contact Number</label>
                                     <div class="form-group">
                                         <input type="number" class="form-input" name="" id="" placeholder="Contact Number"/>
                                     </div>
                                 </div>
                                 <!-- Address -->
                                 <div class="col-lg-12 col-md-12">
+                                    <label for="comunity-name">Address</label>
                                     <div class="form-group">
                                         <input type="text" class="form-input" name="name" id="name" placeholder="Address"/>
                                     </div>
                                 </div>
+                                
+                                <div class="col-lg-12 col-md-12 mt-30">
+                                    <label for="comunity-name">Upload Proof of Identity</label>
+                                    <!-- Upload Docs -->
+                                    <div class="form-group choose-file mb-20">
+                                        <input type="file" class="form-control-file d-inline" id="input-file" multiple="">
+                                    </div>
+                                </div>
 
                             </div>
-                            <!-- Image -->
+                            <!-- Image 
                             <div class="col-md-4 offset-md-1 col-lg-3 offset-lg-0">
                                 <div class="form-group" style="padding: 0%;">
                                     <img src="images/user/user-thumb.jpg" alt="">
                                 </div>
-                                <!-- Upload Image -->
+                                
                                 <div class="form-group choose-file mb-20">
                                     <input type="file" class="form-control-file d-inline" id="input-file">
                                  </div>
                                 <label for="comunity-name">Upload Proof of Identity</label>
-                                <!-- Upload Docs -->
                                 <div class="form-group choose-file mb-20">
                                     <input type="file" class="form-control-file d-inline" id="input-file" multiple="">
                                 </div>
-                            </div>
+                            </div> -->
                         </div>
                         <!-- Sign Up button -->
-                        <div class="form-group">
+                        <div class="form-group mt-30">
                             <input type="submit" name="submit" id="submit" class="form-submit" value="Sign up"/>
                         </div>
                     </form>
