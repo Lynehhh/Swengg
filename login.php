9c5ee9af68b46141034f717968638071308d5df5
Lynehhh <55583598+Lynehhh@users.noreply.github.com>
2019-12-13 10:33:51 +0800

    UPDATE

--- login.php
+++ login.php
@@ -39,7 +39,7 @@ if (isset($_POST['Login']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>JANSY COMMERCIAL</title>
+	<title>GOGO BIYAHE</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
